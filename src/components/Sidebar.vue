ebceabea9234d2f9d129338b94293eb2d5608f03
백재우 <9712jw@gmail.com>
2025-07-30 22:44:49 +0900

    사이드 바 컴포넌트 생성

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <nav class="sidebar">
+      <ul>
+        <li>
+          <router-link to="#">질문&답변</router-link>
+        </li>
+        <li>
+          <router-link to="#">내 활동</router-link>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
