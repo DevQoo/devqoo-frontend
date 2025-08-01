ae7bd996cae96016e6cb98a150ab92bab3a6cfc3
ohdonggeon <o.d.g@kakao.com>
2025-08-01 23:23:47 +0900

    Fix: fontawesome main.js 수정

--- src/main.js
+++ src/main.js
@@ -1,21 +1,21 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
-import "./style.css";
-import App from "./App.vue";
-import router from "./router";
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import './style.css'
+import App from './App.vue'
+import router from './router'
 
-const app = createApp(App);
-const pinia = createPinia();
+const app = createApp(App)
+const pinia = createPinia()
 
-app.use(pinia);
-app.use(router);
+app.use(pinia)
+app.use(router)
 
 // Fontawesome
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { faUser, faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUser, faCoffee } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faUser, faCoffee);
-app.component("font-awesome-icon", FontAwesomeIcon);
+library.add(faUser, faCoffee)
+app.component('font-awesome-icon', FontAwesomeIcon)
 
-app.mount("#app");
+app.mount('#app')
